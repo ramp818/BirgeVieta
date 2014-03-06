2a5bf4ad1d8f84b5c7f39be7ab378c96ce763db9
Ruben Pacheco <pachecaso9436@gmail.com>
2014-03-06 17:41:14 -0600

    Ya (casi) hace divisiòn sintetica

--- BirgeVieta/main.cpp
+++ BirgeVieta/main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 
 using namespace std;
+int Division(int polinomio[],int grado,int x)
+{
+    int aux=polinomio[grado],division[grado];
+    division[grado]=aux;
+    for(int i=grado+1;i>=0;i--)
+    {
+        division[i]=polinomio[i]+aux;
+        aux=0;
+        aux=division[i]*x;
+    }
+
+    return division[0];
+}
 
 int main()
 {
-    int grado,iteraciones,error;
+    int grado;
+    int iteraciones,error;
     cout<<"Grado del polinomio"<<endl;
     cin>>grado;
     int polinomio[grado];
@@ -19,6 +33,7 @@ int main()
     cout<<endl;
     cout<<"Toleracia de error: ";
     cin>>error;
+    cout<<Division(polinomio,grado,error)<<endl;
 
     return 0;
 }
